265113bec42aabec4f4274ad8aeb4107efade844
Ayase <166034664+AyaseFile@users.noreply.github.com>
2025-05-31 23:51:31 +0800

    fix: btop overlay pkg

--- overlays/btop.nix
+++ overlays/btop.nix
@@ -1,13 +1,12 @@
 { pkgs, ... }:
 
 let
-  btop = pkgs.btop;
   btop-darwin = pkgs.callPackage ../packages/btop-darwin { };
 in
 {
   nixpkgs.overlays = [
     (final: prev: {
-      btop = if pkgs.stdenv.isDarwin then btop-darwin else btop;
+      btop = if pkgs.stdenv.isDarwin then btop-darwin else pkgs.btop;
     })
   ];
 }
